3f9d16bf9254075352ed959e662c8249248941e0
Shirel Marino <shirelmr12@gmail.com>
2026-04-30 14:45:55 -0600

    fixes for env

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const cn = {
 const db = pgp(cn);
 
 const app = express();
-app.use(cors({origin : 'http://localhost:5173', credentials: true}));
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
